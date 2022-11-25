a508f6b2a48972887512d17d4a60c78e26bf432a
vavevntgh <supakorn.kongk@gmail.com>
2022-11-25 21:35:12 +0700

    Set unity file
    test animation

---
+++ Assets/swim/Swim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour
+{
+    public Animator anim;
+
+    public float speed = 0.5f;
+    public float rotationSpeed = 100.0f;
+    public float currentSpeed = 0.0f;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        currentSpeed *= translation;
+        anim.SetFloat("speed", currentSpeed);
+        transform.Rotate(0,rotation,0);
+
+        if(translation != 0)
+        {
+            anim.SetBool("Swimming", true);
+        }
+        else
+        {
+            anim.SetBool("Swimming", false);
+            currentSpeed = 0;
+        }
+    }
+}
